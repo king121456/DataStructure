e19ae6d0620a4c5fff683a074470a49411e535ee
king121456 <1025924929@qq.com>
2017-06-23 14:19:22 +0800

    简单的链表和迭代器

---
+++ DataStru/LinkList.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace DataStru
+{
+    /// <summary>
+    /// 链表的实现
+    /// </summary>
+    public class LinkListNode
+    {
+        public Object Value { get; private set; }
+
+
+        public LinkListNode(Object value)
+        {
+            this.Value = value;
+        }
+
+        //直接前驱
+        public LinkListNode Next { get; internal set; }
+        //直接后继
+        public LinkListNode Prev { get; internal set; }
+    }
+
+    public class LinkList
+    {
+        public LinkListNode First { get; set; }
+        public LinkListNode Last { get; set; }
+
+        public LinkListNode Add(Object obj)
+        {
+            LinkListNode node = new LinkListNode(obj);
+            if (First == null)
+            {
+                First = node;
+                Last = node;
+            }
+            //维护next和prev，直接前驱，直接后继
+            else
+            {
+                LinkListNode lastNode=this.Last;
+                this.Last.Next = node;
+                this.Last = node;
+                node.Prev = lastNode;
+            }
+            return node;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            LinkListNode current=this.First;
+            while(current!=null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+           
+        }
+    }
+
+
+}
---
+++ DataStru/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStru
+{
+    //c#数据结构
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LinkList x = new LinkList();
+            //x.value = "xxx";
+            LinkList list = new LinkList();
+            list.Add(1);
+            list.Add(2);
+            list.Add("fffff");
+            //var类型推断
+            foreach (var x in list)
+            {
+                Console.Write(x);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
